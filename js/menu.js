3e92812e4369ee53c90a73286e082ec21c819830
Gesa Trojan <gesa.trojan@gmail.com>
2021-11-28 21:01:11 +0100

    new commit

--- js/menu.js
+++ js/menu.js
@@ -6,10 +6,10 @@ $.ajax({
                 $.each(data, function(key, drink) {
                 console.log(drink.Title)
                  $('#coffees').append(`<div class="col-md-3 text-center">
-                    <img src="${drink.PicURL}" alt="" class="img-fluid rounded-circle">
-                     <h3>${drink.Title}</h3>
+                 <img src="${drink.PicURL}" alt="" class="img-fluid">   
+                 <h3>${drink.Title}</h3>
                     <p>€ ${drink.Price}</p>
-                     <p>${drink.Description}</p>
+                    <p>${drink.Description}</p>
                  </div>`);
             })
         }
